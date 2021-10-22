395c814a80d0d06ab97611384087839f0ef92967
Paul Traynor <2205382t@student.gla.ac.uk>
2021-10-22 15:02:29 +0100

    started remote_endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod remote_endpoint;
+
 fn main() {
     println!("Hello, world!");
+    let mut remote_endpoint = remote_endpoint::RemoteEndpoint::new();
+    remote_endpoint.with_hostname("www.google.com".to_string());
+    
 }
---
+++ src/remote_endpoint.rs
@@ -0,0 +1,28 @@
+use std::net::Ipv4Addr;
+use std::net::Ipv6Addr;
+
+pub struct RemoteEndpoint {
+    pub hostname: Option<String>,
+    service: Option<String>,
+    ipv4: Option<Ipv4Addr>,
+    ipv6: Option<Ipv6Addr>,
+    port: Option<String>,
+    interface: Option<String>,
+}
+
+impl RemoteEndpoint {
+    pub fn new() -> RemoteEndpoint {
+        RemoteEndpoint {
+            hostname: None,
+            service: None,
+            ipv4: None,
+            ipv6: None,
+            port: None,
+            interface: None,
+        }
+    }
+
+    pub fn with_hostname(&mut self, hostname: String) {
+        self.hostname = Some(hostname);
+    }
+}
