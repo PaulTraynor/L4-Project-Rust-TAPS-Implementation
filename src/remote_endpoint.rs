9721e582acf9df401bba4907dea33df38007d021
Paul Traynor <2205382t@student.gla.ac.uk>
2021-11-01 17:50:38 +0000

    refactored RemoteEndpoint into enum

--- src/remote_endpoint.rs
+++ src/remote_endpoint.rs
@@ -1,26 +1,9 @@
 use std::net::Ipv4Addr;
 use std::net::Ipv6Addr;
 
-pub struct RemoteEndpoint {
-    pub hostname: Option<String>,
-    service: Option<String>,
-    pub ipv4: Option<Ipv4Addr>,
-    ipv6: Option<Ipv6Addr>,
-    pub port: Option<u16>,
+pub enum RemoteEndpoint {
+    HostnamePort(String, u16),
+    Ipv4Port(Ipv4Addr, u16),
+    Ipv6Port(Ipv4Port, u16),
 }
 
-impl RemoteEndpoint {
-    pub fn new() -> RemoteEndpoint {
-        RemoteEndpoint {
-            hostname: None,
-            service: None,
-            ipv4: None,
-            ipv6: None,
-            port: None,
-        }
-    }
-
-    pub fn with_hostname(&mut self, hostname: String) {
-        self.hostname = Some(hostname);
-    }
-}
