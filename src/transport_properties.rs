61036c532a99f930cbeb5a1d65b8674de847c444
Paul Traynor <2205382t@student.gla.ac.uk>
2021-11-29 19:34:05 +0000

    added relevant selection properties and security parameters

--- src/transport_properties.rs
+++ src/transport_properties.rs
@@ -5,22 +5,18 @@ struct TransportProperties {
 }
 
 enum SelectionProperty {
-
+    Reliability(Preference),
+    PreserveMsgBoundaries(Preference),
+    PreserveOrder(Preference),
+    Multistreaming(Preference),
+    CongestionControl(Preference),
 }
 
-enum ConnectionProperty {
-
-}
+enum ConnectionProperty {}
 
 struct SecurityParameters {
-    identity: Option<String>,
-    key_pair: Option<(String, String)>,
-    supported_group: Option<String>,
-    ciphersuite: Option<String>,
-    signature_algorithm: Option<String>,
-    pre_shared_key: Option<String>,
-    max_cached_sessions: Option<u32>,
-    cached_session_liefetime_seconds: Option<u32>,
+    certificate: Option<String>,
+    private_key: Option<String>,
 }
 
 enum Preference {
