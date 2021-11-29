50b206182da0bc1d43a667c96d4346af8182b68f
Paul Traynor <2205382t@student.gla.ac.uk>
2021-11-29 14:46:55 +0000

    cargo.lock and cargo.toml changes

--- src/pre_connection.rs
+++ src/pre_connection.rs
@@ -1,33 +1,33 @@
+use crate::connection::*;
 use crate::local_endpoint;
 use crate::remote_endpoint;
-use std::net::TcpStream;
-use dns_lookup::{lookup_host};
-use crate::connection::*;
+use dns_lookup::lookup_host;
+use tokio::net::TcpStream;
 
 pub struct PreConnection {
     pub local_endpoint: local_endpoint::LocalEndpoint,
     pub remote_endpoint: remote_endpoint::RemoteEndpoint,
-
 }
 
 impl PreConnection {
-    fn initiate(&self) -> Connection {
+    async fn initiate(&self) -> Connection {
         // candidate gathering...
         // candidate racing...
-        let stream = TcpStream::connect("127.0.0.1:8080").unwrap();
-        
-        let tcp_connection = Box::new (TcpConnection {stream: stream}) ;
-        Connection {protocol_impl: tcp_connection}
+        let stream = TcpStream::connect("127.0.0.1:8080").await.unwrap();
 
+        let tcp_connection = Box::new(TcpConnection { stream: stream });
+        Connection {
+            protocol_impl: tcp_connection,
+        }
     }
 
     //fn listen(&self) -> Listener {}
-} 
+}
 
 pub fn get_ips(hostname: &str) -> Vec<std::net::IpAddr> {
-    //let ips: Vec<std::net::IpAddr> = 
+    //let ips: Vec<std::net::IpAddr> =
     lookup_host(hostname).unwrap()
     //for ip in ips {
-      //  println!("{}", ip);
+    //  println!("{}", ip);
     //}
 }
