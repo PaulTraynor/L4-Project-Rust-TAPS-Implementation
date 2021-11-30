745b3893b67bd2d88f4fbc1fa168baec7df81010
Paul Traynor <2205382t@student.gla.ac.uk>
2021-11-30 17:17:09 +0000

    updated pre_connection

--- src/pre_connection.rs
+++ src/pre_connection.rs
@@ -1,16 +1,32 @@
 use crate::connection::*;
-use crate::local_endpoint;
-use crate::remote_endpoint;
+use crate::endpoint;
+use crate::transport_properties;
 use dns_lookup::lookup_host;
 use tokio::net::TcpStream;
 
 pub struct PreConnection {
-    pub local_endpoint: local_endpoint::LocalEndpoint,
-    pub remote_endpoint: remote_endpoint::RemoteEndpoint,
+    pub local_endpoint: Option<endpoint::LocalEndpoint>,
+    pub remote_endpoint: Option<endpoint::RemoteEndpoint>,
+    pub transport_properties: transport_properties::TransportProperties,
+    pub security_parameters: Option<transport_properties::SecurityParameters>,
 }
 
 impl PreConnection {
-    async fn initiate(&self) -> Connection {
+    pub fn new(
+        local_endpoint: Option<endpoint::LocalEndpoint>,
+        remote_endpoint: Option<endpoint::RemoteEndpoint>,
+        transport_properties: transport_properties::TransportProperties,
+        security_parameters: Option<transport_properties::SecurityParameters>,
+    ) -> PreConnection {
+        PreConnection {
+            local_endpoint: local_endpoint,
+            remote_endpoint: remote_endpoint,
+            transport_properties: transport_properties,
+            security_parameters: security_parameters,
+        }
+    }
+
+    pub async fn initiate(&self) -> Connection {
         // candidate gathering...
         // candidate racing...
         let stream = TcpStream::connect("127.0.0.1:8080").await.unwrap();
