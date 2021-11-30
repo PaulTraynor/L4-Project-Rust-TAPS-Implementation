052c61e7b8a84c5fcb1eeb9bd12e2400a37cab3d
Paul Traynor <2205382t@student.gla.ac.uk>
2021-11-30 17:06:02 +0000

    updated transport property methods

--- src/transport_properties.rs
+++ src/transport_properties.rs
@@ -1,7 +1,19 @@
 struct TransportProperties {
     selectionProperties: Vec<SelectionProperty>,
     connectionProperties: Vec<ConnectionProperty>,
-    securityParameters: SecurityParameters,
+}
+
+impl TransportProperties {
+    pub fn new() -> TransportProperties {
+        TransportProperties {
+            selectionProperties: Vec::new(),
+            connectionProperties: Vec::new(),
+        }
+    }
+
+    pub fn add_selection_property(&mut self, selectionProperty: SelectionProperty) {
+        self.selectionProperties.push(selectionProperty);
+    }
 }
 
 enum SelectionProperty {
@@ -15,8 +27,17 @@ enum SelectionProperty {
 enum ConnectionProperty {}
 
 struct SecurityParameters {
-    certificate: Option<String>,
-    private_key: Option<String>,
+    certificate_path: String,
+    private_key_path: String,
+}
+
+impl SecurityParameters {
+    pub fn new(path_1: String, path_2: String) -> SecurityParameters {
+        SecurityParameters {
+            certificate_path: path_1,
+            private_key_path: path_2,
+        }
+    }
 }
 
 enum Preference {
