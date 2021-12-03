589abc00d1b6a64ab339e1f8ad4bb49a4c4f5782
Paul Traynor <2205382t@student.gla.ac.uk>
2021-12-03 12:28:20 +0000

    started implementation of quinn

--- src/connection.rs
+++ src/connection.rs
@@ -1,16 +1,26 @@
+use crate::endpoint::LocalEndpoint;
+use crate::error;
+use crate::pre_connection::PreConnection;
 use async_trait::async_trait;
 use env_logger;
 use mio_quic;
 use quiche;
+use quinn;
 use ring::rand::*;
 use std::convert::TryFrom;
 use std::io;
 use std::io::{BufReader, Read, Write};
 use std::net::Shutdown;
-use std::net::SocketAddr;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::net::{Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
 use std::sync::{Arc, Mutex};
+use std::{
+    fs,
+    net::ToSocketAddrs,
+    path::PathBuf,
+    time::{Duration, Instant},
+};
 use tokio;
 use tokio::io::{copy, split, AsyncReadExt, AsyncWriteExt};
 use tokio_rustls::rustls::{self, OwnedTrustAnchor};
@@ -75,185 +85,51 @@ impl ProtocolConnection for TcpConnection {
     }
 }
 
+pub const ALPN_QUIC_HTTP: &[&[u8]] = &[b"hq-29"];
+
 pub struct QuicConnection {
     pub conn: std::pin::Pin<Box<quiche::Connection>>,
 }
 
 impl QuicConnection {
-    pub async fn connect(
-        addr: SocketAddr,
-    ) -> tokio::task::JoinHandle<Option<std::pin::Pin<Box<quiche::Connection>>>> {
-        const MAX_DATAGRAM_SIZE: usize = 1350;
-        let mut buf = [0; 65535];
-        let mut out = [0; MAX_DATAGRAM_SIZE];
-        // Setup the event loop.
-        let poll = mio_quic::Poll::new().unwrap();
-        let mut events = mio_quic::Events::with_capacity(1024);
-
-        // Bind to INADDR_ANY or IN6ADDR_ANY depending on the IP family of the
-        // server address. This is needed on macOS and BSD variants that don't
-        // support binding to IN6ADDR_ANY for both v4 and v6.
-        let bind_addr = match addr {
-            std::net::SocketAddr::V4(_) => "0.0.0.0:0",
-            std::net::SocketAddr::V6(_) => "[::]:0",
-        };
-
-        println!("{:?}", bind_addr);
-
-        // Create the UDP socket backing the QUIC connection, and register it with
-        // the event loop.
-        let socket = std::net::UdpSocket::bind(bind_addr).unwrap();
-        let socket = mio_quic::net::UdpSocket::from_socket(socket).unwrap();
-        poll.register(
-            &socket,
-            mio_quic::Token(0),
-            mio_quic::Ready::readable(),
-            mio_quic::PollOpt::edge(),
-        )
-        .unwrap();
-
-        // Create the configuration for the QUIC connection.
-        let mut config = quiche::Config::new(quiche::PROTOCOL_VERSION).unwrap();
-
-        // *CAUTION*: this should not be set to `false` in production!!!
-        config.verify_peer(false);
-
-        config
-            .set_application_protos(b"\x0ahq-interop\x05hq-29\x05hq-28\x05hq-27\x08http/0.9")
-            .unwrap();
-
-        config.set_max_idle_timeout(5000);
-        config.set_max_recv_udp_payload_size(MAX_DATAGRAM_SIZE);
-        config.set_max_send_udp_payload_size(MAX_DATAGRAM_SIZE);
-        config.set_initial_max_data(10_000_000);
-        config.set_initial_max_stream_data_bidi_local(1_000_000);
-        config.set_initial_max_stream_data_bidi_remote(1_000_000);
-        config.set_initial_max_stream_data_uni(1_000_000);
-        config.set_initial_max_streams_bidi(100);
-        config.set_initial_max_streams_uni(100);
-        config.set_disable_active_migration(true);
-
-        // Generate a random source connection ID for the connection.
-        let mut scid = [0; quiche::MAX_CONN_ID_LEN];
-        SystemRandom::new().fill(&mut scid[..]).unwrap();
-
-        let scid = quiche::ConnectionId::from_ref(&scid);
-
-        // Create a QUIC connection and initiate handshake.
-        let mut conn = quiche::connect(None, &scid, addr, &mut config).unwrap();
-
-        println!(
-            "connecting to {:} from {:} with scid ...",
-            addr,
-            socket.local_addr().unwrap(),
-            //hex_dump(&scid)
-        );
-
-        let (write, send_info) = conn.send(&mut out).expect("initial send failed");
-        while let Err(e) = socket.send_to(&out[..write], &send_info.to) {
-            if e.kind() == std::io::ErrorKind::WouldBlock {
-                println!("send() would block");
-                continue;
-            }
-            panic!("send() failed: {:?}", e);
+    pub async fn connect(addr: SocketAddr, pre_connection: PreConnection) -> Option<String> {
+        let mut roots = rustls::RootCertStore::empty();
+        match pre_connection.security_parameters {
+            Some(files) => match fs::read(&files.certificate_path) {
+                Ok(v) => match roots.add(&rustls::Certificate(v)) {
+                    Ok(_) => {}
+                    Err(_) => return None,
+                },
+                Err(e) => return None,
+            },
+            None => return None,
         }
+        let mut client_crypto = rustls::ClientConfig::builder()
+            .with_safe_defaults()
+            .with_root_certificates(roots)
+            .with_no_client_auth();
+        client_crypto.alpn_protocols = ALPN_QUIC_HTTP.iter().map(|&x| x.into()).collect();
 
-        println!("written {}", write);
-        //let potential_conn =
-        return tokio::spawn(async move {
-            loop {
-                poll.poll(&mut events, conn.timeout()).unwrap();
-
-                // Read incoming UDP packets from the socket and feed them to quiche,
-                // until there are no more packets to read.
-                'read: loop {
-                    // If the event loop reported no events, it means that the timeout
-                    // has expired, so handle it without attempting to read packets. We
-                    // will then proceed with the send loop.
-                    if events.is_empty() {
-                        println!("timed out");
-                        conn.on_timeout();
-                        break 'read;
-                    }
-
-                    let (len, from) = match socket.recv_from(&mut buf) {
-                        Ok(v) => v,
-                        Err(e) => {
-                            // There are no more UDP packets to read, so end the read
-                            // loop.
-                            if e.kind() == std::io::ErrorKind::WouldBlock {
-                                println!("recv() would block");
-                                break 'read;
-                            }
-                            panic!("recv() failed: {:?}", e);
-                        }
-                    };
-
-                    println!("got {} bytes", len);
-
-                    let recv_info = quiche::RecvInfo { from };
-
-                    // Process potentially coalesced packets.
-                    let read = match conn.recv(&mut buf[..len], recv_info) {
-                        Ok(v) => v,
-
-                        Err(e) => {
-                            println!("recv failed: {:?}", e);
-                            continue 'read;
-                        }
-                    };
-
-                    println!("processed {} bytes", read);
+        let local_endpoint = match pre_connection.local_endpoint {
+            Some(v) => match v {
+                LocalEndpoint::Ipv4Port(ip, port) => SocketAddr::from(SocketAddrV4::new(ip, port)),
+                LocalEndpoint::Ipv6Port(ip, port) => {
+                    SocketAddr::from(SocketAddrV6::new(ip, port, 0, 0))
                 }
+            },
+            None => SocketAddr::from(SocketAddrV6::new(
+                Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1),
+                0,
+                0,
+                0,
+            )),
+        };
+        let mut endpoint = quinn::Endpoint::client(local_endpoint).unwrap();
+        endpoint.set_default_client_config(quinn::ClientConfig::new(Arc::new(client_crypto)));
 
-                println!("done reading");
-
-                if conn.is_closed() {
-                    println!("connection closed, {:?}", conn.stats());
-                    return None;
-                }
-
-                if conn.is_established() {
-                    return Some(conn);
-                }
-
-                // Generate outgoing QUIC packets and send them on the UDP socket, until
-                // quiche reports that there are no more packets to be sent.
-                loop {
-                    let (write, send_info) = match conn.send(&mut out) {
-                        Ok(v) => v,
-
-                        Err(quiche::Error::Done) => {
-                            println!("done writing");
-                            break;
-                        }
-
-                        Err(e) => {
-                            println!("send failed: {:?}", e);
-
-                            conn.close(false, 0x1, b"fail").ok();
-                            break;
-                        }
-                    };
-
-                    if let Err(e) = socket.send_to(&out[..write], &send_info.to) {
-                        if e.kind() == std::io::ErrorKind::WouldBlock {
-                            println!("send() would block");
-                            break;
-                        }
-
-                        panic!("send() failed: {:?}", e);
-                    }
-
-                    println!("written {}", write);
-                }
-            }
-        });
-        //let mut out = potential_conn.await.unwrap();
-        //return out;
+        return None;
     }
 }
-
 #[async_trait]
 impl ProtocolConnection for QuicConnection {
     async fn send(&mut self, buffer: &[u8]) {
